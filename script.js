c119cfbadf36046a7e5a6087221e02cef2281551
Adam Delora <deloraa@gmail.com>
2021-11-16 21:02:58 -0600

    added image array

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var imagelinks = ["images/amafraid.jpg", "images/amfeelingsick.jpg", "images/aminpain.jpg","images/amangry.jpg","images/amfrustrated.jpg","images/amsad.jpg","images/amchoking.jpg","images/amhotcold.jpg","images/amshortofbreath.jpg","images/amdizzy.jpg","images/amhungrythirsty.jpg","images/amtired.jpg","images/wanthobupdown.jpg","images/wanttvvideo.jpg","images/wanttobecomforted.jpg","images/wantliedown.jpg","images/wantquiet.jpg","images/wanttobesucctioned.jpg","images/wantlightsoffon.jpg","images/wantremote.jpg","images/wanttogohome.jpg","images/wantwater.jpg","images/wantsitup.jpg","images/wanttosleep.jpg"];
+
+var imageid=[]
 webgazer.setGazeListener((data,timestamp)=>{
-  console.log(data,timestamp);
+//  console.log(data,timestamp);
 }).begin()
