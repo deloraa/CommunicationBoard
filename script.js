8214fb44441ea524f43652e9780ad248ecb00824
Adam Delora <deloraa@gmail.com>
2021-11-16 21:09:12 -0600

    added image id tags

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var imagelinks = ["images/amafraid.jpg", "images/amfeelingsick.jpg", "images/aminpain.jpg","images/amangry.jpg","images/amfrustrated.jpg","images/amsad.jpg","images/amchoking.jpg","images/amhotcold.jpg","images/amshortofbreath.jpg","images/amdizzy.jpg","images/amhungrythirsty.jpg","images/amtired.jpg","images/wanthobupdown.jpg","images/wanttvvideo.jpg","images/wanttobecomforted.jpg","images/wantliedown.jpg","images/wantquiet.jpg","images/wanttobesucctioned.jpg","images/wantlightsoffon.jpg","images/wantremote.jpg","images/wanttogohome.jpg","images/wantwater.jpg","images/wantsitup.jpg","images/wanttosleep.jpg"];
 
-var imageid=[]
+var imageid=["img000","img001","img002","img010","img011","img012","img020","img021","img022","img030","img031","img032","img100","img101","img102","img110","img111","img112","img120","img121","img122","img130","img131","img132"];
 webgazer.setGazeListener((data,timestamp)=>{
 //  console.log(data,timestamp);
 }).begin()
