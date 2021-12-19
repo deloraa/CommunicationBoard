f10025bc4de00e8642c3f570d7833e65cf2d5b73
Adam Delora <deloraa@gmail.com>
2021-12-19 15:30:14 -0600

    added raw facemesh files

--- dist/script.js
+++ dist/script.js
@@ -659,7 +659,7 @@ async function onResults(results) {
 */
 }
 const faceMesh = new FaceMesh({locateFile: (file) => {
-    return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh@0.4.1633559619/${file}`;
+    return `./${file}`;
   }});
 faceMesh.setOptions(solutionOptions);
 
