c7467e8d69284b81f6302d776f69118f38837f12
Adam Delora <deloraa@gmail.com>
2021-12-19 15:07:16 -0600

    added version numbers for sanity

--- dist/script.js
+++ dist/script.js
@@ -659,7 +659,7 @@ async function onResults(results) {
 */
 }
 const faceMesh = new FaceMesh({locateFile: (file) => {
-    return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh@0.4.1633559619/${file}`;
   }});
 faceMesh.setOptions(solutionOptions);
 
