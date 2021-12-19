8c58ba790824d1e74094bba5178ade4cac24ad46
Adam Delora <deloraa@gmail.com>
2021-12-19 10:41:07 -0600

    added more script info

--- dist/script.js
+++ dist/script.js
@@ -31,6 +31,126 @@ function testSupport(supportedDevices) {
             `is not well supported at this time, continue at your own risk.`);
     }
 }
+
+//between 0.00-0.5
+var widthThreshold = 0.08;
+var LOOK_DELAY = 300; // 0.5 second
+var verticalThreshold = -0.11;
+var LOOK_UP_DELAY = 200;
+var upperBlinkCutoff = 5.5;
+var lowerBlinkCutoff = 4;
+
+var lookleftrightsensslider = document.getElementById("lookleftrightsensslider");
+var lookleftrightsens = document.getElementById("lookleftrightsens");
+lookleftrightsens.innerHTML = lookleftrightsensslider.value; // Display the default slider value
+lookleftrightsensslider.oninput = function() {
+    lookleftrightsens.innerHTML = this.value;
+    widthThreshold = parseFloat(this.value);
+}
+
+var lookupsensslider = document.getElementById("lookupsensslider");
+var lookupsens = document.getElementById("lookupsens");
+lookupsens.innerHTML = lookupsensslider.value; // Display the default slider value
+lookupsensslider.oninput = function() {
+    lookupsens.innerHTML = this.value;
+    verticalThreshold = -1 * parseFloat(this.value);
+}
+
+var timetoactivateslider = document.getElementById("timetoactivateslider");
+var timetoactivate = document.getElementById("timetoactivate");
+timetoactivate.innerHTML = timetoactivateslider.value; // Display the default slider value
+timetoactivateslider.oninput = function() {
+    timetoactivate.innerHTML = this.value;
+    LOOK_DELAY = parseInt(this.value);
+}
+
+var timetoactivatelookupslider = document.getElementById("timetoactivatelookupslider");
+var timetoactivatelookup = document.getElementById("timetoactivatelookup");
+timetoactivatelookup.innerHTML = timetoactivatelookupslider.value; // Display the default slider value
+timetoactivatelookupslider.oninput = function() {
+    timetoactivatelookup.innerHTML = this.value;
+    LOOK_UP_DELAY = parseInt(this.value);
+}
+
+var soundButton = document.getElementById('soundButton');
+var soundOnOff = false;
+soundButton.onclick = () => {
+    if (soundOnOff) {
+        soundOnOff = false;
+        imagelinks[31] = "Icons/32-SoundOff.jpeg"
+        soundButton.className = "btn btn-outline-secondary"
+        soundButton.innerHTML = '<img src="images/volume-mute.svg" width="16" height="16" class="bi bi-volume-mute" viewBox="0 0 16 16"></img>Sound Off'
+        $(rightImages).each(function(i) {
+            if ($(this).attr("src") === "Icons/32-SoundOn.jpeg") {
+                $(this).attr("src", "Icons/32-SoundOff.jpeg")
+            }
+        });
+        $(leftImages).each(function(i) {
+            if ($(this).attr("src") === "Icons/32-SoundOn.jpeg") {
+                $(this).attr("src", "Icons/32-SoundOff.jpeg")
+            }
+        });
+
+    } else {
+        soundOnOff = true;
+        imagelinks[31] = "Icons/32-SoundOn.jpeg"
+        soundButton.className = "btn btn-outline-primary"
+        soundButton.innerHTML = '<img src="images/volume-up-fill.svg" width="16" height="16" class="bi bi-volume-mute" viewBox="0 0 16 16"></img>Sound On'
+        $(rightImages).each(function(i) {
+            if ($(this).attr("src") === "Icons/32-SoundOff.jpeg") {
+                $(this).attr("src", "Icons/32-SoundOn.jpeg")
+            }
+        });
+        $(leftImages).each(function(i) {
+            if ($(this).attr("src") === "Icons/32-SoundOff.jpeg") {
+                $(this).attr("src", "Icons/32-SoundOn.jpeg")
+            }
+        });
+    }
+}
+
+var bluetooth = document.getElementById('bluetooth');
+var bluetoothbutton = document.getElementById('bluetoothButton');
+let toggleLightCharacteristic;
+let bluetoothDevice;
+const DEVICE_NAME = 'ESP32_Bluetooth';
+const SEND_SERVICE = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+const SEND_SERVICE_CHARACTERISTIC = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+var bluetoothConnected = false;
+bluetoothbutton.onclick = () => {
+    if (bluetoothConnected === false) {
+        if (!navigator.bluetooth) {
+            alert('Sorry, your browser doesn\'t support Bluetooth API');
+            return;
+        }
+        navigator.bluetooth.requestDevice({
+                acceptAllDevices: true,
+                optionalServices: [SEND_SERVICE]
+            })
+            .then(device => {
+                bluetoothDevice = device;
+
+                return device.gatt.connect();
+            })
+            .then(server => server.getPrimaryService(SEND_SERVICE))
+            .then(service => service.getCharacteristic(SEND_SERVICE_CHARACTERISTIC))
+            .then(characteristic => {
+                toggleLightCharacteristic = characteristic;
+                bluetoothConnected = true;
+                bluetoothbutton.className = "btn btn-outline-primary"
+                bluetoothbutton.innerHTML = '<img src="images/bluetoothOn.svg" width="16" height="16" viewBox="0 0 16 16"></img>Bluetooth Connected';
+                //toggleLightCharacteristic.writeValue(Uint8Array.of(1));
+            })
+            .catch(error => {
+                console.error(error);
+                bluetoothConnected = false;
+                bluetoothbutton.className = "btn btn-outline-secondary"
+                bluetoothbutton.innerHTML = '<img src="images/bluetoothOff.svg" width="16" height="16" viewBox="0 0 16 16"></img>Bluetooth Connected';
+            });
+    }
+
+}
+
 const controls = window;
 const drawingUtils = window;
 const mpFaceMesh = window;
